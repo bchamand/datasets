17fe5dd66704dee0854dbf7c79ed94df81d6ac72
Benjamin Chamand <10047581+bchamand@users.noreply.github.com>
2021-01-21 17:36:38 +0100

    Corrects the name of the file to import

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,6 +1,6 @@
-from datasets.speech_commands import SpeechCommandsDataset
+from datasets.speech_commands import SpeechCommands, SpeechCommandsV1
 
 __version__ = "0.1.0-dev0"
-__all__ = ("SpeechCommandsDataset",)
+__all__ = ("SpeechCommands",)
 
 del speech_commands
